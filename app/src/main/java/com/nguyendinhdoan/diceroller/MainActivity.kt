c514cc221d0c9ed828bd6eba3634515de275f5a0
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-30 14:53:59 +0700

    find views effective

--- app/src/main/java/com/nguyendinhdoan/diceroller/MainActivity.kt
+++ app/src/main/java/com/nguyendinhdoan/diceroller/MainActivity.kt
@@ -9,26 +9,33 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var rollButton: Button
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        rollButton = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener {
-            val randomInt = Random().nextInt(6) + 1
-            val drawableResource = when (randomInt) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-            }
-
-            diceImage.setImageResource(drawableResource)
+           rollDice()
         }
 
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
 }
