d5c4d2f4f38bf730b0720a164fa07e4b6126eb71
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-30 09:59:04 +0700

    display a random number

--- app/src/main/java/com/nguyendinhdoan/diceroller/MainActivity.kt
+++ app/src/main/java/com/nguyendinhdoan/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.nguyendinhdoan.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,13 +14,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
+        val resultText: TextView = findViewById(R.id.result_text)
         rollButton.setOnClickListener {
-            rollDice()
+            val randomInt = Random().nextInt(6) + 1
+            resultText.text = randomInt.toString()
         }
 
     }
 
-    private fun rollDice() {
-        Toast.makeText(this, "button clicked", Toast.LENGTH_LONG).show()
-    }
 }
