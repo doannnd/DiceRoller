6727d7ae2d962378de274ee05cde657b1ac62b9e
doannd <nguyendinhdoanjdk@gmail.com>
2019-04-30 14:44:10 +0700

    add and update image resource

--- app/src/main/java/com/nguyendinhdoan/diceroller/MainActivity.kt
+++ app/src/main/java/com/nguyendinhdoan/diceroller/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.nguyendinhdoan.diceroller
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,10 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val resultText: TextView = findViewById(R.id.result_text)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
         rollButton.setOnClickListener {
             val randomInt = Random().nextInt(6) + 1
-            resultText.text = randomInt.toString()
+            val drawableResource = when (randomInt) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            diceImage.setImageResource(drawableResource)
         }
 
     }
